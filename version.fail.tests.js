8f62c63b94483688aea7363f5bff7be5dbe6d395
ravila <rafa.avim@gmail.com>
2015-10-07 13:57:30 -0500

    prueba de fallo para la cuenta de 240xHr al servicio de version

--- version.fail.tests.js
+++ version.fail.tests.js
@@ -6,40 +6,40 @@
 
 var EventEmitter = require('events').EventEmitter;
 var request = require('request');
-var utilities = require('./utilities');
-var errorCounter = counter = 0;
+var config = require('./config');
+var uuid = require('uuid');
+var errorCounter = 0;
+var counter = 0;
 var calloptions = {
-  url: 'http://ravila-apigee-test.nearbpo.com/v1/crapfolder/version',
+  url: config.serviceURL.version,
   headers: {
-    'Token-Auth': 'ravila@nearbpo.com'
+    'Token-Auth': uuid.v4()
   }
 };
 var emitter = new EventEmitter();
-
-emitter.on('increment', checkCounter);
-
-function checkCounter () {
-  console.log('counter = ' + counter);
-  if (counter === 500) {
+emitter.on('increment', function checkCounter () {
+  console.log('counter = ' + counter + "  |||| errorCounter = " + errorCounter);
+  if (counter === 300) { // limite de 300 llamadas
     console.log("let's clear interval :)");
     clearInterval(failInterval);
+  } else if (errorCounter === 5) {
+    console.log("let's clear interval :), we got lots of expected errors :D");
+    clearInterval(failInterval);
   }
-}
+});
 
 var callApi = function () {
-  request.get(calloptions)
-    .on('response', function onResponse(response) {
-      console.log(response.statusCode);
+  request.get(calloptions, function (error, response, body) {
+    console.log(response.statusCode);
+    if (!error && response.statusCode >= 200 && response.statusCode <= 299) {
       counter++;
-      emitter.emit('increment');
-    })
-    .on('error', function onError(err) {
-      console.error("ups! :(");
-      console.error(err);
-			console.log('clearing interval cuz we had an error!!!');
+    } else if (response.statusCode === config.statusCodes.quotaViolation) {
       errorCounter++;
-      emitter.emit('increment');
-    });
+    }
+
+    emitter.emit('increment');
+  });
 };
 
+// se mandara una llamada a la api cada 100 milisegundos.
 var failInterval = setInterval(callApi, 100);
